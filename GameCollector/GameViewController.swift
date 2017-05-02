ba74edc7ac12c9f723e239e2441d9e3c60c30ddb
Peter Rooney <pfrjr1@me.com>
2017-05-02 09:12:58 -0400

    end of lesson 76 all good so far

---
+++ GameCollector/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  GameCollector
+//
+//  Created by Peter Rooney on 5/2/17.
+//  Copyright © 2017 Peter Rooney. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var gameImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
